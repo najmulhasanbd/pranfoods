62186bb809299b5a8f962fa58fce07f71bc4b45e
najmul57 <najmulcse247@gmail.com>
2024-11-20 10:58:15 +0600

    project start

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+    "use strict";
+
+
+    // scrollToTop
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+        topSpeed: 300, // Speed back to top (ms)
+        animation: 'fade', // Fade, slide, none
+        animationInSpeed: 200, // Animation in speed (ms)
+        animationOutSpeed: 200, // Animation out speed (ms)
+        scrollText: '<i class="fa-solid fa-arrow-up"></i>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
+
+
+
+})(jQuery);
